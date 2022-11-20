8ef037cb8db15b25a08205cc017152a2a68cfb09
Dragos Mateo Damoc <dragosd@newro.co>
2022-11-20 19:54:10 +0200

    first commit

---
+++ components/Assignements.js
@@ -0,0 +1,54 @@
+import AssignmentList from "./AssignementList.js";
+import AssignmentCreate from "./AssignmentCreate.js";
+
+export default {
+  components: { AssignmentList, AssignmentCreate },
+
+  template: `
+        <section class="space-y-6">
+            <assignment-list :assignments="filters.inProgress" title="In Progress"></assignment-list>
+            <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
+            
+            <assignment-create @add="add"></assignment-create>
+        </section>
+    `,
+
+  data() {
+    return {
+      assignments: [
+        // { name: "Finish project", complete: false, id: 1, tag: "math" },
+        // { name: "Read Chapter 4", complete: false, id: 2, tag: "science" },
+        // { name: "Turn in Homework", complete: false, id: 3, tag: "math" },
+      ],
+    };
+  },
+
+  computed: {
+    filters() {
+      return {
+        inProgress: this.assignments.filter(
+          (assignment) => !assignment.complete
+        ),
+        completed: this.assignments.filter((assignment) => assignment.complete),
+      };
+    },
+  },
+
+  created() {
+    fetch("http://localhost:3000/assignments")
+      .then((response) => response.json())
+      .then((assignments) => {
+        this.assignments = assignments;
+      });
+  },
+
+  methods: {
+    add(name) {
+      this.assignments.push({
+        name: name,
+        complete: false,
+        id: this.assignments.length + 1,
+      });
+    },
+  },
+};
