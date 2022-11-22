bc314b59e7b379846a65dd12fe0dbe351c1e9cab
Dragos Mateo Damoc <dragosd@newro.co>
2022-11-22 18:10:06 +0200

    server

--- components/App.js
+++ components/App.js
@@ -1,8 +1,19 @@
 import Assignements from "./Assignements.js";
+import Panel from "./Panel.js";
 
 export default {
   components: {
     Assignements,
+    Panel,
   },
-  template: `<assignements></assignements>`,
+  template: `
+      <div class="grid gap-6">
+          <assignements></assignements
+       </panel>
+      </div>
+  `,
+
+  props: {
+    add: Object,
+  },
 };
